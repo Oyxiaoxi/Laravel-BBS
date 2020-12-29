26974eee4fd9674a986c9dfbcd1478f9903ca0df
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2020-12-29 09:28:28 +0000

    PasswordRest

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -20,7 +20,7 @@ class EventServiceProvider extends ServiceProvider
         ],
         \Illuminate\Auth\Events\Verified::class => [
             \App\Listeners\EmailVerified::class,
-        ],
+        ]
     ];
 
     /**
