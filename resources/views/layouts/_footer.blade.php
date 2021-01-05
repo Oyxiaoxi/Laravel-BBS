002bdb2f3ba4cebb8324f2e5b5a66afc789c10d9
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2021-01-05 20:34:55 +0000

    Site

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="http://weibo.com/AmOyxiaoxi" target="_blank">Oyxiaoxi</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="float-right"><a href="mailto:Oyxiaoxi@hotmail.com">联系我们</a></p>
+        <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
