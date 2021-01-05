c98bc24909a09bc23fa407ef5090b0ebaf80e5df
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2021-01-05 21:32:56 +0000

    Last Actived Time

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use HasRoles, HasFactory, MustVerifyEmailTrait, Traits\ActiveUserHelper;
+    use HasRoles, HasFactory, MustVerifyEmailTrait, Traits\ActiveUserHelper, Traits\LastActivedAtHelper;
 
     use Notifiable {
         notify as protected laravelNotify;
