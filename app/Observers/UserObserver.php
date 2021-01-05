0a67d12060bc7985cc19ef0bbd0d7b3e33b01706
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2021-01-05 21:38:27 +0000

    Users Default Avatar

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,12 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://img.zcool.cn/community/013678554324500000019ae9b46f2b.jpg@1280w_1l_2o_100sh.jpg';
+        }
+    }
 }
