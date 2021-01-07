3e7610584b5cbac1c6430d264b5e4d7af58788e9
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2021-01-07 02:19:03 +0000

    JWT

--- routes/api.php
+++ routes/api.php
@@ -27,13 +27,22 @@ Route::prefix('v1')
                 // 用户注册
                 Route::post('users', 'UsersController@store')
                     ->name('users.store');
+                // 图片验证码
+                Route::post('captchas', 'CaptchasController@store')
+                    ->name('captchas.store');
                 // 第三方登录
                 Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
                     ->where('social_type', 'wechat')
                     ->name('socials.authorizations.store');
-                // 图片验证码
-                Route::post('captchas', 'CaptchasController@store')
-                    ->name('captchas.store');
+                // 登录
+                Route::post('authorizations', 'AuthorizationsController@store')
+                    ->name('authorizations.store');
+                // 刷新token
+                Route::put('authorizations/current', 'AuthorizationsController@update')
+                    ->name('authorizations.update');
+                // 删除token
+                Route::delete('authorizations/current', 'AuthorizationsController@destroy')
+                    ->name('authorizations.destroy');
             });
 
         Route::middleware('throttle:' . config('api.rate_limits.access'))
